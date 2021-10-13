4f9aaecf7aeb9db2c92431abba5a231aea8c38a8
Kurt Heimerman <kurtheimerman@yahoo.com>
2021-10-12 21:31:07 -0700

    started manager questions

--- index.js
+++ index.js
@@ -1,5 +1,45 @@
-console.log('hi')
+// Imports
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+
+const getManagerInput = () => {
+    return inquirer.prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Enter manager name:',
+      },
+      {
+        type: 'input',
+        name: 'employeeId',
+        message: 'Enter manager employee id:',
+      },
+      {
+        type: 'input',
+        name: 'emailAddress',
+        message: 'Enter manager email address:',
+      },
+      {
+        type: 'input',
+        name: 'officeNumber',
+        message: 'Enter manager office number:',
+      }
+    ])
+}
 
 
 
-
+// .then(({ title, description }) => {
+//   appData.title = title;
+//   appData.description = description;
+// });
+const start = () => {
+    // deleteOldFiles();
+    getManagerInput()
+    .then( ( data ) => {
+        console.log(data)
+    })
+    .catch();
+}
+start();
