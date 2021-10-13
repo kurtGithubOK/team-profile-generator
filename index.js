6ea8f3237804f461015888dc85b4b6f0a58641ab
Kurt Heimerman <kurtheimerman@yahoo.com>
2021-10-12 21:59:25 -0700

    menu options now displayed

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 // Imports
 const inquirer = require('inquirer');
 const fs = require('fs');
+const Manager = require('./lib/Manager')
 
+// Constants.
+const ADD_ENGINEER = 'Add Engineer';
+const ADD_INTERN = 'Add Intern';
+const DONE = 'Done';
 
-const getManagerInput = () => {
+// Questions for entering manager info.
+const getManagerInfo = () => {
     return inquirer.prompt([
       {
         type: 'input',
@@ -17,7 +23,7 @@ const getManagerInput = () => {
       },
       {
         type: 'input',
-        name: 'emailAddress',
+        name: 'email',
         message: 'Enter manager email address:',
       },
       {
@@ -26,19 +32,63 @@ const getManagerInput = () => {
         message: 'Enter manager office number:',
       }
     ])
+};
+
+// Prompt manager to add an engineer, intern, or done.
+const displayMenuOptions = () => {
+    return inquirer.prompt([
+        {
+            type: 'list',
+            name: 'menuChoice',
+            message: 'What would you like to do next?',
+            choices: [ADD_ENGINEER, ADD_INTERN, DONE],
+        }
+    ]);
 }
 
 
+// Utilities to create employee objects.
+const makeManagerObject = data => {
+    const { employeeId, name, email, officeNumber } = data;
+    const manager = new Manager(employeeId, name, email, officeNumber);
+    return manager;
+}
 
-// .then(({ title, description }) => {
-//   appData.title = title;
-//   appData.description = description;
-// });
+// List of employees to be displayed on html page.
+const employees = [];
+
 const start = () => {
     // deleteOldFiles();
-    getManagerInput()
+    getManagerInfo()
     .then( ( data ) => {
-        console.log(data)
+        // Make manager object.
+        const manager = makeManagerObject(data);
+        // Add to list of employees.
+        employees.push(manager);
+
+        // Display menu
+        displayMenuOptions()
+        .then( (menuSelection) => {
+            if(menuSelection === ADD_ENGINEER) {
+                console.log('engineer chosen')
+
+            } else if(menuSelection === ADD_INTERN) {
+                console.log('intern chosen')
+
+            } else {
+                // Manager chose 'done'.
+                console.log('engineer chosen')
+            }
+
+        })
+        .catch();
+
+
+        console.log(manager)
+
+        // if engineer ...
+        // if intern ...
+        // if done ...
     })
     .catch();
 }
