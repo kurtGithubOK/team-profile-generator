e7dff434bdaa46a0d794b194057739ab10fdc416
Kurt Heimerman <kurtheimerman@yahoo.com>
2021-10-23 16:47:52 -0700

    Starting to work!

--- index.js
+++ index.js
@@ -15,112 +15,139 @@ const outputPathAndFilename = './dist/myTeam.html';
 let employees = [];  // List of employees to be displayed on html page.
 
 const start = () => {
-    promptForInput()
-    // .then(() => {
-    // writeHtmlDoc();
-    // })
-    // .catch((err) => console.error('An error occured when prompting user for input:', err));
+  promptForInput()
+  // .then(() => {
+  // writeHtmlDoc();
+  // })
+  // .catch((err) => console.error('An error occured when prompting user for input:', err));
 };
 
 const promptForInput = () => {
-    inquirer.prompt(managerQuestions)
-        .then((response) => {
-            const manager = new Manager(response);
-            employees.push(manager);
-            displayMenuOptions();
-        })
-        .catch((error) => console.log('Error occurred when prompting manager questions:', error));
+  inquirer.prompt(managerQuestions)
+    .then((response) => {
+      const manager = new Manager(response);
+      employees.push(manager);
+      displayMenuOptions();
+    })
+    .catch((error) => console.log('Error occurred when prompting manager questions:', error));
 };
 
 const displayMenuOptions = () => {
-    inquirer.prompt(menuOptions)
-        .then(({ menuChoice }) => {
-            if (menuChoice === ADD_ENGINEER) {
-                // if engineer ...
-                inquirer.prompt(engineerQuestions)
-                    .then((engineerAnswers) => {
-                        const engineer = new Engineer(engineerAnswers);
-                        employees.push(engineer);
-                        displayMenuOptions();
-                    })
-                    .catch((error) => console.log('Error occurred when prompting engineer questions:', error));
-            } else if (menuChoice === ADD_INTERN) {
-                console.log('intern chosen')
-                // if intern ...
-                inquirer.prompt(internQuestions)
-                    .then((internAnswers) => {
-                        const intern = new Intern(internAnswers);
-                        employees.push(intern);
-                        displayMenuOptions();
-                    })
-                    .catch();
-            } else {
-                // if done ...
-                console.log('done chosen')
-            }
-        })
-        .catch();
+  inquirer.prompt(menuOptions)
+    .then(({ menuChoice }) => {
+      if (menuChoice === ADD_ENGINEER) {
+        // if engineer ...
+        inquirer.prompt(engineerQuestions)
+          .then((engineerAnswers) => {
+            const engineer = new Engineer(engineerAnswers);
+            employees.push(engineer);
+            displayMenuOptions();
+          })
+          .catch((error) => console.log('Error occurred when prompting engineer questions:', error));
+      } else if (menuChoice === ADD_INTERN) {
+        console.log('intern chosen')
+        // if intern ...
+        inquirer.prompt(internQuestions)
+          .then((internAnswers) => {
+            const intern = new Intern(internAnswers);
+            employees.push(intern);
+            displayMenuOptions();
+          })
+          .catch();
+      } else {
+        // if done ...
+        console.log('done chosen')
+        writeHtmlDoc();
+      }
+    })
+    .catch();
 };
 
 const writeHtmlDoc = () => {
-    Promise.all([readTemplate(templateTop), makeEmployeeCards(), readTemplate(templateBottom)])
-    .then( (data) => {
-        console.log('kkkkkkkkkkk', data)
-        fs.writeFile(outputPathAndFilename, data, (error) => {
-            if (error) reject(`Error occurred in writeContent()`, error);
-        });
+  Promise.all([readTemplate(templateTop), makeEmployeeCards(), readTemplate(templateBottom)])
+    .then((data) => {
+      fs.writeFile(outputPathAndFilename, data.join(''), (error) => {
+        if (error) reject(`Error occurred in writeContent()`, error);
+      });
     })
-    .catch( (error) => console.log('Error occurred in writeHtmlDoc()', error));
+    .catch((error) => console.log('Error occurred in writeHtmlDoc()', error));
 };
 
 const readTemplate = (templateFile) => {
-    return new Promise((resolve, reject) => {
-        fs.readFile(templateFile, 'utf8', (error, data) => {
-            if (error) reject('Error occurred in readTemplateTop():', error);
-            else resolve(data);
-        });
+  return new Promise((resolve, reject) => {
+    fs.readFile(templateFile, 'utf8', (error, data) => {
+      if (error) reject('Error occurred in readTemplateTop():', error);
+      else resolve(data);
     });
+  });
 };
 
 let employeeCards = '';
 const makeEmployeeCards = () => {
-    return new Promise((resolve, reject) => {
-        for (const employee of employees) {
-            // pass employee to template and get content back.
-            const employeeCard = makeEmployeeCard(employee);
-            employeeCards += employeeCard;
-            console.log('ddddddddd', employeeCards)
-        }
-        resolve(employeeCards);
-    });
+  return new Promise((resolve, reject) => {
+    for (const employee of employees) {
+      // pass employee to template and get content back.
+      const employeeCard = makeEmployeeCard(employee);
+      employeeCards += employeeCard;
+    }
+    resolve(employeeCards);
+  });
 
 };
 
 // Returns html card for an employee.
-const makeEmployeeCard = ( {employeeId, name, email, github}) => `
-    <div class="col card">
+const makeEmployeeCard = ({ employeeId, name, email, officeNumber, github, school }) => {
+  return `
+    <div class="col-3 card">
     <div class="card-header bg-primary">
         ${name}<br/>
         <img src="http://openweathermap.org/img/wn/04d@2x.png" /><br/>
-        employee type
+        ${makeEmployeeTypeString(officeNumber, github, school)}
     </div>
     <ul class="list-group list-group-flush">
         <li class="list-group-item">ID: ${employeeId}</li>
-        <li class="list-group-item">Email: ${email}   MAKE LINK!</li>
-        <li class="list-group-item">GitHub: ${github}   MAKE LINK!</li>
+        <li class="list-group-item"><a mailto:${email}>${email}</a></li>
+        ${makeCardThirdLine(officeNumber, github, school)}
     </ul>
     </div>
-`;
+`};
+
+const makeEmployeeTypeString = (officeNumber, github, school) => {
+    let employeeTypeString = '';
+    if (officeNumber) {
+      employeeTypeString = 'Manager';
+    } else if (github) {
+        employeeTypeString = 'Engineer';
+    } else {
+      employeeTypeString = `Intern`;
+    }
+    return employeeTypeString;
+  };
+
+// If officeNumber is truthy, its a manager.  Similar for thers.
+const makeCardThirdLine = (officeNumber, github, school) => {
+  const openingTag = '<li class="list-group-item">';
+  const closingTag = '</li>';
+  let content = '';
+  if (officeNumber) {
+    content = 'Office Number: ' + officeNumber;
+  } else if (github) {
+    content = `<a href="https://github.com/${github}">github</a>`;
+  } else {
+    content = `School: ${school}`;
+  }
+  return openingTag + content + closingTag;
+};
 
 // start();
-writeHtmlDoc();
-// const data = {employeeId: 22, name: 'kurt', email: 'myEmail', officeNumber: 333}
-// const manager = new Manager(data);
-// employees.push(manager)
+const data = { employeeId: 22, name: 'kurt', email: 'myEmail', officeNumber: 333, github: 'myGithub' }
+const manager = new Manager(data);
+employees.push(manager)
 // makeEmployeeCards()
-// .then( (contents) => {
-//     console.log(contents)
+// .then((contents) => {
+//     console.log('eeeee', contents)
 // });
 
+writeHtmlDoc();
 
 
